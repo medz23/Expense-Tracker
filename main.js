b88e3b0c93d38db3939839c6a6d34f7c243341e6
Mogoase Ana-Maria Luisa <m.mogoase23@gmail.com>
2020-11-05 21:57:26 +0200

    total income expense

--- main.js
+++ main.js
@@ -31,12 +31,35 @@ function addTransactionsDOM(transaction) {
   list.appendChild(item);
 
 }
+//update balance,income, expense
+function updateValues() {
+  const amounts = transactions.map(transaction => transaction.amount);
 
+  const total = amounts.reduce((acc, item) => (acc += item),0)
+  .toFixed(2);
+
+  const income = amounts
+                .filter(item => item > 0)
+                .reduce((acc,item) => (acc += item), 0)
+                .toFixed(2);
+
+  const expense = (amounts
+                .filter(item => item < 0)
+                .reduce((acc,item) => (acc += item), 0) * -1).toFixed(2);
+
+  balance.innerText = `$${total}`;
+  moneyPlus.innerText = `$${income}`;
+  moneyMinus.innerText = `$${expense}`;
+
+
+}
 //init app
 function init() {
   list.innerHTML = '';
 
+
   transactions.forEach(addTransactionsDOM);
+  updateValues();
 }
 
 init();
